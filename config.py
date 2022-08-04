0f1bfc6cec1fba31977c4ad615d2e67e7ddb8beb
Mike Kertser <76855241+kertser@users.noreply.github.com>
2022-08-04 17:55:55 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ true_url = 'Data/DataSet_TRUE.csv'
 false_url = 'Data/DataSet_FAKE.csv'
 
 emData_url = 'Data/Emotions/NRC-Emotion-Intensity-Lexicon-v1.txt'
-wordsData_url = '/Data/Emotions/NRC-Emotion-Lexicon-v0.92-InManyLanguages.xlsx'
+wordsData_url = 'Data/Emotions/NRC-Emotion-Lexicon-v0.92-InManyLanguages.xlsx'
